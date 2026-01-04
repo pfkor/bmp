eed0b205055caa53d395f9133a5f1ea29e10b1ca
Platon <pfkoryaka@edu.hse.ru>
2026-01-04 21:15:18 +0300

    Добавил проверку валидности данных

--- color.c
+++ color.c
@@ -4,9 +4,17 @@
 #include "color.h"
 
 Color pixel_to_color(Pixel_24 pixel){
+    if (pixel.r < 0 || pixel.r >= 256 || pixel.g < 0 \
+     || pixel.g >= 256 || pixel.b < 0 || pixel.b >= 256){
+        return (Color){0, 0, 0};
+    }
     return (Color){(float) pixel.r, (float) pixel.g, (float) pixel.b};
 }
 
 Pixel_24 color_to_pixel(Color color){
+    if (color.r < 0 || color.r >= 256 || color.g < 0 \
+     || color.g >= 256 || color.b < 0 || color.b >= 256){
+        return (Pixel_24){0, 0, 0};
+    }
     return (Pixel_24){(uint8_t) color.b, (uint8_t) color.g, (uint8_t) color.r};
 }
